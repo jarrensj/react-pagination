151f248b22019096f47780ac2fdb3e5e7001dac9
jarrensj <sanjose.jarren@gmail.com>
2019-06-23 17:20:09 -0700

    get posts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Posts from './components/Posts';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
-    <div className="container">
-      <h1>My App</h1>
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">My Blog</h1>
+      <Posts posts={posts} loading={loading} />
     </div>
   );
 }
